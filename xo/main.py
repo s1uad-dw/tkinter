d6a1d4600d899a7897cf03fd71f2ba5b535ef691
s1uad-dw <s1uad.dw@gmail.com>
2023-06-01 12:51:48 +0300

    formatting with Pylint

--- xo/main.py
+++ xo/main.py
@@ -1,8 +1,10 @@
-from tkinter import *
-from tkinter import messagebox
-from PIL import Image, ImageTk
+'''Модуль, обеспеччивающий работу с графическим интерфейсом'''
+from tkinter import Tk, Button, messagebox
 import os
 from functools import partial
+from PIL import ImageTk, Image
+
+
 
 WINS = [
     [1, 2, 3],
@@ -30,29 +32,29 @@ root.configure(bg='#ffffff')
 # root.iconbitmap(take_path(['icon.ico']))
 root.geometry('600x600')
 
-current_player = 'X'
+CURRENT_PLAYER = 'X'
 
-clear_btn_img = ImageTk.PhotoImage(Image.open(
+CLEAR_BTN_IMG = ImageTk.PhotoImage(Image.open(
     take_path(['static', 'pictures', 'clear.png'])))
-x_btn_img = ImageTk.PhotoImage(Image.open(
+X_BTN_IMG = ImageTk.PhotoImage(Image.open(
     take_path(['static', 'pictures', 'x.png'])))
-o_btn_img = ImageTk.PhotoImage(Image.open(
+O_BTN_IMG = ImageTk.PhotoImage(Image.open(
     take_path(['static', 'pictures', 'o.png'])))
 
 
 def click(button_pos: tuple):
     '''Обрабатывает нажатие на кнопку'''
-    global current_player
-    if current_player == 'X':
-        buttons[button_pos].configure(image=x_btn_img)
-        current_player = 'O'
-    elif current_player == 'O':
-        buttons[button_pos].configure(image=o_btn_img)
-        current_player = 'X'
+    global CURRENT_PLAYER
+    if CURRENT_PLAYER == 'X':
+        buttons[button_pos].configure(image=X_BTN_IMG)
+        CURRENT_PLAYER = 'O'
+    elif CURRENT_PLAYER == 'O':
+        buttons[button_pos].configure(image=O_BTN_IMG)
+        CURRENT_PLAYER = 'X'
     check()
 
 
-def isSublist(list1, list2):
+def is_sublist(list1, list2):
     '''Проверяет наличие всех элементов list1 в list2'''
     for elem in list1:
         if elem not in list2:
@@ -60,56 +62,56 @@ def isSublist(list1, list2):
     return True
 
 
-def clear(result):
+def clear():
     '''Очищает игровое поле'''
-    global current_player
+    global CURRENT_PLAYER
     for button in buttons.values():
-        button.configure(image=clear_btn_img)
-        current_player = 'X'
+        button.configure(image=CLEAR_BTN_IMG)
+    CURRENT_PLAYER = 'X'
 
 
 def check():
     '''Проверяет наличие выигрышных комбинаций на поле'''
-    global x_btn_img, o_btn_img
     x_btns = []
     o_btns = []
     for button in buttons.items():
         button_number = button[0][0]+1+button[0][1]+button[0][0]*2
-        if button[1]['image'] == x_btn_img._PhotoImage__photo.name:
+        if button[1]['image'] == X_BTN_IMG._PhotoImage__photo.name:
             x_btns.append(button_number)
-        elif button[1]['image'] == o_btn_img._PhotoImage__photo.name:
+        elif button[1]['image'] == O_BTN_IMG._PhotoImage__photo.name:
             o_btns.append(button_number)
     for win in WINS:
-        if isSublist(win, x_btns):
+        if is_sublist(win, x_btns):
             result = messagebox.askyesno(title='Игра окончена',
-                                         message='Победили крестики!\nХотите сыграть еще раз?')
+                        message='Победили крестики!\nХотите сыграть еще раз?')
             if result == 1:
-                clear(result)
+                clear()
             elif result == 0:
                 root.quit()
-        elif isSublist(win, o_btns):
+        elif is_sublist(win, o_btns):
             result = messagebox.askyesno(title='Игра окончена',
                         message='Победили нолики!\nХотите сыграть еще раз?')
             if result == 1:
-                clear(result)
+                clear()
             elif result == 0:
                 root.quit()
 
 
-buttons = dict()
+buttons = {}
 for row in range(3):
     for col in range(3):
-        current_button = Button(root,
-                                image=clear_btn_img,
-                                background='#ffffff',
-                                highlightbackground='#ffffff',
-                                highlightcolor='#ffffff',
-                                activebackground='#ffffff',
-                                borderwidth=0,
-                                width=180,
-                                height=180,
-                                command=partial(click, (row, col))
-                                )
+        current_button = Button(
+            root,
+            image=CLEAR_BTN_IMG,
+            background='#ffffff',
+            highlightbackground='#ffffff',
+            highlightcolor='#ffffff',
+            activebackground='#ffffff',
+            borderwidth=0,
+            width=180,
+            height=180,
+            command=partial(click, (row, col))
+        )
         buttons[row, col] = current_button
         current_button.grid(row=row, column=col, padx=8, pady=8)
 
